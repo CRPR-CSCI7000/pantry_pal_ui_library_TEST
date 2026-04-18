efa4751c0754971a6409e901fff3797e220d4abf
rlodhicode <rayaan.lodhi@gmail.com>
2026-04-18 15:47:02 -0600

    modal buttons prop interface adjustment

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,7 @@
  *   modal_id  - string key in modalState redux slice
  *   title     - dialog title string
  *   children  - dialog body
- *   buttons   - array of { text, onClick, variant }
+ *   footerButtons   - array of { text, onClick, variant }
  *   style     - optional style overrides for the Paper
  *
  * Peer dependency: react-redux store must contain a `modalState` slice
@@ -23,7 +23,7 @@ import {
   Button,
 } from "@mui/material";
 
-const Modal = ({ modal_id, title, children, buttons = [], style }) => {
+const Modal = ({ modal_id, title, children, footerButtons = [], style }) => {
   const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.modalState[modal_id]);
 
@@ -57,7 +57,7 @@ const Modal = ({ modal_id, title, children, buttons = [], style }) => {
       </DialogContent>
       <Divider sx={{ marginBottom: 1 }} />
       <DialogActions sx={{ justifyContent: "flex-end", flexShrink: 0 }}>
-        {buttons.map(({ text, onClick, variant }, index) => (
+        {footerButtons.map(({ text, onClick, variant }, index) => (
           <Button
             key={index}
             variant={variant}
